4d8cb9082a41c76ed76c39cfa78f2ab4bd700acb
DevrajDC <devrajchatribin9978@gmail.com>
2024-04-06 13:55:52 +0530

    Initial project setup and code Editor

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CodeEditor from "./components/CodeEditor";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <main className="dark min-h-screen flex justify-center items-center bg-neutral-950 text-white">
+      <CodeEditor />
+    </main>
+  );
+}
+
+export default App;
