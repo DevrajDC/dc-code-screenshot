7a179d80393c566e529f367b735fc327953443e0
DevrajDC <devrajchatribin9978@gmail.com>
2024-04-06 21:42:16 +0530

    Width measurement functionality with reset button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { themes, fonts } from "./options";
 import useStore from "./store";
 import { cn } from "./lib/utils";
@@ -12,8 +12,15 @@ import FontSizeInput from "./components/controls/FontSizeInput";
 import PaddingSlider from "./components/controls/PaddingSlider";
 import BackgroundSwitch from "./components/controls/BackgroundSwitch";
 import DarkModeSwitch from "./components/controls/DarkModeSwitch";
+import { Resizable } from "re-resizable";
+import { Button } from "./components/ui/button";
+import { ResetIcon } from "@radix-ui/react-icons";
+import WidthMeasurement from "./components/WidthMeasurement";
 
 function App() {
+  const [width, setWidth] = useState("auto");
+  const [showWidth, setShowWidth] = useState(false);
+
   const theme = useStore((state) => state.theme);
   const padding = useStore((state) => state.padding);
   const fontStyle = useStore((state) => state.fontStyle);
@@ -48,18 +55,42 @@ function App() {
         href={fonts[fontStyle].src}
         crossOrigin="anonymous"
       />
-      <div
-        className={cn(
-          "overflow-hidden mb-2 transition-all ease-out",
-          showBackground ? themes[theme].background : "ring ring-neutral-900"
-        )}
-        style={{ padding }}
-        ref={editorRef}
-      >
-        <CodeEditor />
-      </div>
 
-      <Card className="fixed bottom-8 py-16 px-8 mx-6 bg-neutral-900/90 backdrop-blur">
+      <Resizable
+        enable={{ left: true, right: true }}
+        minWidth={padding * 2 + 300}
+        size={{ width }}
+        onResize={(e, dir, ref) => setWidth(ref.offsetWidth)}
+        onResizeStart={() => setShowWidth(true)}
+        onResizeStop={() => setShowWidth(false)}
+      >
+        <div
+          className={cn(
+            "overflow-hidden mb-2 transition-all ease-out",
+            showBackground ? themes[theme].background : "ring ring-neutral-900"
+          )}
+          style={{ padding }}
+          ref={editorRef}
+        >
+          <CodeEditor />
+        </div>
+        <WidthMeasurement showWidth={showWidth} width={width} />
+        <div
+          className={cn(
+            "transition-opacity w-fit mx-auto -mt-4",
+            showWidth || width === "auto"
+              ? "invisible opacity-0"
+              : "visible opacity-100"
+          )}
+        >
+          <Button size="sm" onClick={() => setWidth("auto")} variant="ghost">
+            <ResetIcon className="mr-2" />
+            Reset width
+          </Button>
+        </div>
+      </Resizable>
+
+      <Card className="fixed bottom-8 py-6 px-8 mx-6 bg-neutral-900/90 backdrop-blur">
         <CardContent className="flex flex-wrap gap-6 p-0">
           <ThemeSelect />
           <LanguageSelect />
@@ -68,7 +99,10 @@ function App() {
           <PaddingSlider />
           <BackgroundSwitch />
           <DarkModeSwitch />
-          <ExportOptions targetRef={editorRef} />
+          <div className="w-px bg-neutral-800" />
+          <div className="place-self-center">
+            <ExportOptions targetRef={editorRef} />
+          </div>
         </CardContent>
       </Card>
     </main>
